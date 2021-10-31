0797610ae2cde3842fdf1b6abb08330738cdebfa
vikash <prajapativikash0000@gmail.com>
2021-10-31 21:50:43 +0530

    refactored naming conventions in homefragment view model

--- app/src/main/java/com/example/theatreapp/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/example/theatreapp/viewmodel/HomeFragmentViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import com.example.theatreapp.App
+import com.example.theatreapp.R
 import com.example.theatreapp.utils.Event
 import com.example.theatreapp.connections.SocketManager
 import com.example.theatreapp.models.requests.User
@@ -15,9 +16,9 @@ import com.example.theatreapp.storage.SessionData
 
 class HomeFragmentViewModel(private var socketManager: SocketManager) : ViewModel() {
     private val TAG: String = HomeFragmentViewModel::class.java.canonicalName
-    private var room = MutableLiveData<String>()
-    private var user = MutableLiveData<String>()
-    public val loading = MutableLiveData<Int>()
+    private var _room = MutableLiveData<String>()
+    private var _user = MutableLiveData<String>()
+    val loading = MutableLiveData<Int>()
     val connectionState = MutableLiveData<String>()
     val joinRoomState = MutableLiveData<String>()
 
@@ -38,6 +39,7 @@ class HomeFragmentViewModel(private var socketManager: SocketManager) : ViewMode
         loading.postValue(View.GONE)
         if(joinedRoomResponse == null){
             socketManager.stopListeningToServer()
+            connectionState.postValue("Failed to join room")
             return@Observer;
         }
         joinRoomState.postValue(App.gson.toJson(joinedRoomResponse))
@@ -53,35 +55,35 @@ class HomeFragmentViewModel(private var socketManager: SocketManager) : ViewMode
     }
 
     private fun clearFields(){
-        room.value = ""
-        user.value = ""
+        _room.value = ""
+        _user.value = ""
     }
 
     private fun updateSessionData(response: JoinedRoomResponse){
-        SessionData.localUser = User(user.value!!).apply { id = response.room.host }
+        SessionData.localUser = User(_user.value!!).apply { id = response.room.host }
         SessionData.currentRoom = response.room
         SessionData.participants = response.room.participants as MutableList<ParticipantsItem>
         Log.i(TAG, "updateSessionData: $SessionData")
     }
 
-    var roomName : String
+    var room : String
         set(value){
-            room.value = value
+            _room.value = value
         }
-        get() = room.value?:""
+        get() = _room.value?:""
 
 
-    var userName : String
+    var user : String
         set(value){
-            user.value = value
+            _user.value = value
         }
-        get() = user.value?:""
+        get() = _user.value?:""
 
 
     var invalidInput = MutableLiveData<Event<String>>()
 
     fun validateInput() {
-        if (room.value.isNullOrEmpty() || user.value.isNullOrEmpty())
+        if (_room.value.isNullOrEmpty() || _user.value.isNullOrEmpty())
             invalidInput.value = Event("Invalid Input")
         else{
             loading.postValue(View.VISIBLE)
@@ -101,6 +103,6 @@ class HomeFragmentViewModel(private var socketManager: SocketManager) : ViewMode
 
     private fun joinRoom() {
         Log.i(TAG, "joinRoom: ")
-        socketManager.joinRoom(user.value!!, room.value!!)
+        socketManager.joinRoom(_user.value!!, _room.value!!)
     }
 }
--- app/src/main/java/com/example/theatreapp/viewmodel/StreamingRoomFragmentViewModel.kt
+++ app/src/main/java/com/example/theatreapp/viewmodel/StreamingRoomFragmentViewModel.kt
@@ -3,15 +3,17 @@ package com.example.theatreapp.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.theatreapp.models.participants.Participant
+import com.example.theatreapp.models.response.joinroomresponse.ParticipantsItem
+import com.example.theatreapp.storage.SessionData
 
 class StreamingRoomFragmentViewModel : ViewModel() {
     // temporarily we are keeping only video url, later we will be needing more video details
     private val activeVideoUrl = MutableLiveData<String>()
     private val connectionState = MutableLiveData<String>()
-    private val participants = MutableLiveData<List<Participant>>()
-    var participantsList : MutableLiveData<List<Participant>> = participants
+    private val participants = MutableLiveData<List<ParticipantsItem>>()
+    var participantsList : MutableLiveData<List<ParticipantsItem>> = participants
 
     init {
-        participants.value = emptyList()
+        participants.value = SessionData.participants
     }
 }
