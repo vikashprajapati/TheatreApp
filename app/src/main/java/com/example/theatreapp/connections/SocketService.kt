ef3f76e5885a1eacd549915b5a30346e2455df44
vikash <prajapativikash0000@gmail.com>
2021-10-31 23:59:04 +0530

    added leave room event for activity finished callback

--- app/src/main/java/com/example/theatreapp/connections/SocketService.kt
+++ app/src/main/java/com/example/theatreapp/connections/SocketService.kt
@@ -2,93 +2,95 @@ package com.example.theatreapp.connections
 
 import com.example.theatreapp.App
 import com.example.theatreapp.constants.SocketConstants
+import com.example.theatreapp.models.requests.LeaveRoomRequest
 import com.example.theatreapp.models.response.joinroomresponse.JoinedRoomResponse
+import com.example.theatreapp.storage.SessionData
 import io.socket.client.IO
 import io.socket.client.Socket
 import java.net.URI
 
 class SocketService : BaseObservable<SocketService.SocketEventListener>() {
 
- private val SOCKET_ENDPOINT = "http://192.168.43.133:5000"
- private var socket : Socket = IO.socket(URI.create(SOCKET_ENDPOINT))
+    private val SOCKET_ENDPOINT = "http://192.168.43.133:5000"
+    private var socket : Socket = IO.socket(URI.create(SOCKET_ENDPOINT))
 
- private fun handleConnection(connectionStatus : String){
-  for (listener in getListeners()){
-   listener.connectionStatus(connectionStatus)
-  }
- }
+    private fun handleConnection(connectionStatus : String){
+        for (listener in getListeners()){
+            listener.connectionStatus(connectionStatus)
+        }
+    }
 
- private fun joinRoom(response :String){
-  for (listener in getListeners()){
-   val response = App.gson.fromJson<JoinedRoomResponse>(response, JoinedRoomResponse::class.java)
-   listener.joinRoomResponse(response)
-  }
- }
+    private fun joinRoom(response :String){
+        for (listener in getListeners()){
+            val response = App.gson.fromJson<JoinedRoomResponse>(response, JoinedRoomResponse::class.java)
+            listener.joinRoomResponse(response)
+        }
+    }
 
- fun initializeSocketAndConnect(){
-  socket.on(Socket.EVENT_CONNECT) {
-   handleConnection(Socket.EVENT_CONNECT)
-  }.on(Socket.EVENT_CONNECT_ERROR) {
-   handleConnection(Socket.EVENT_CONNECT_ERROR)
-  }.on(Socket.EVENT_DISCONNECT) {
-   handleConnection(Socket.EVENT_DISCONNECT)
-  }.on(SocketConstants.videoPlayed){
-   for (listener in getListeners()){
-    listener.playEvent()
-   }
-  }.on(SocketConstants.videoPaused){
-   for (listener in getListeners()){
-    listener.pauseEvent()
-   }
-  }.on(SocketConstants.previousVideo){
-   for (listener in getListeners()){
-    listener.previousVideoEvent()
-   }
-  }.on(SocketConstants.nextVideo){
-   for (listener in getListeners()){
-    listener.nextVideoEvent()
-   }
-  }.on(SocketConstants.videoSynced){
-   for (listener in getListeners()){
-    listener.syncVideoEvent()
-   }
-  }.on(SocketConstants.participantJoined){
-   for (listener in getListeners()){
-    listener.newParticipantJoinedEvent()
-   }
-  }.on(SocketConstants.participantLeft){
-   for (listener in getListeners()){
-    listener.userLeft()
-   }
-  }.on(SocketConstants.roomJoined){
-   joinRoom(it[0].toString())
-  }
+    fun initializeSocketAndConnect(){
+        socket.on(Socket.EVENT_CONNECT) {
+            handleConnection(Socket.EVENT_CONNECT)
+        }.on(Socket.EVENT_CONNECT_ERROR) {
+            handleConnection(Socket.EVENT_CONNECT_ERROR)
+        }.on(Socket.EVENT_DISCONNECT) {
+            handleConnection(Socket.EVENT_DISCONNECT)
+        }.on(SocketConstants.videoPlayed){
+            for (listener in getListeners()){
+                listener.playEvent()
+            }
+        }.on(SocketConstants.videoPaused){
+            for (listener in getListeners()){
+                listener.pauseEvent()
+            }
+        }.on(SocketConstants.previousVideo){
+            for (listener in getListeners()){
+                listener.previousVideoEvent()
+            }
+        }.on(SocketConstants.nextVideo){
+            for (listener in getListeners()){
+                listener.nextVideoEvent()
+            }
+        }.on(SocketConstants.videoSynced){
+            for (listener in getListeners()){
+                listener.syncVideoEvent()
+            }
+        }.on(SocketConstants.participantJoined){
+            for (listener in getListeners()){
+                listener.newParticipantJoinedEvent()
+            }
+        }.on(SocketConstants.participantLeft){
+            for (listener in getListeners()){
+                listener.userLeft()
+            }
+        }.on(SocketConstants.roomJoined){
+            joinRoom(it[0].toString())
+        }
 
-  socket.connect()
- }
+        socket.connect()
+    }
 
- fun send(eventType : String, params: Any){
-  // serialize args
-  val params = App.gson.toJson(params)
-  socket.emit(eventType, params)
- }
+    fun send(eventType : String, params: Any){
+        // serialize args
+        val params = App.gson.toJson(params)
+        socket.emit(eventType, params)
+    }
 
- fun disconnectSocket(){
-  socket.disconnect()
- }
+    fun disconnectSocket(){
+        socket.emit("leave room")
+    }
 
- fun isConnected() : Boolean = socket.connected()
+    fun isConnected() : Boolean = socket.connected()
 
- interface SocketEventListener{
-  fun playEvent()
-  fun pauseEvent()
-  fun previousVideoEvent()
-  fun nextVideoEvent()
-  fun syncVideoEvent()
-  fun roomJoinedEvent()
-  fun newParticipantJoinedEvent()
-  fun connectionStatus(eventConnect: String)
-  fun joinRoomResponse(joinedRoomResponse: JoinedRoomResponse)
-  fun userLeft()
- }
+    interface SocketEventListener{
+        fun playEvent()
+        fun pauseEvent()
+        fun previousVideoEvent()
+        fun nextVideoEvent()
+        fun syncVideoEvent()
+        fun roomJoinedEvent()
+        fun newParticipantJoinedEvent()
+        fun connectionStatus(eventConnect: String)
+        fun joinRoomResponse(joinedRoomResponse: JoinedRoomResponse)
+        fun userLeft()
+    }
 }
---
+++ app/src/main/java/com/example/theatreapp/models/requests/LeaveRoomRequest.kt
@@ -0,0 +1,5 @@
+package com.example.theatreapp.models.requests
+
+import com.example.theatreapp.models.response.joinroomresponse.Room
+
+class LeaveRoomRequest(room: Room, user: User)
